d4091a6a73c92495543dc63d54f09a30de4b9254
Pablo Salvi <salvipablo@gmail.com>
2021-11-16 11:55:48 -0300

    Agregado de mock de tareas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,22 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "25/11/2021",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Terminar segundo modulo de Angular",
+        day: "10/12/2021",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Actualizar documentacion de lo estudiado",
+        day: "22/12/2021",
+        reminder: true
+    }
+]
